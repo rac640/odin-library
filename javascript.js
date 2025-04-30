4100bee8a72ad380c34cde2aafad369149dce5e7
rac640 <50179707+rac640@users.noreply.github.com>
2025-04-30 10:42:07 -0400

    updated addBookToLibrary function: made Book Constructor into variable, and pushed variable into myLibrary array

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ function Book(name) {
   this.name = name;
 //   Testing to see if this returns customized name
   console.log(name);
+  return name;
 }
 
 
@@ -22,11 +23,14 @@ function Book(name) {
 function addBookToLibrary(bookName) {
   // take params (ex: name), create a book then store it in the array
   // To Test: Make this into a variable and push it into array (Ex: let coolBook = Book(bookName); push coolBook into myLibrary)
-    Book(bookName);
+    let coolBook = Book(bookName);
 
     // Next Step: Add Book object to myLibrary Array 
     // To test this, run addBookToLibrary and make it return updated myLibrary array.  
+    // To add object to array: https://stackoverflow.com/questions/6254050/how-to-add-an-object-to-an-array
+    myLibrary.push(coolBook);
 }
 
 addBookToLibrary("Hahaha");
 addBookToLibrary("Abc");
+addBookToLibrary("Harry Potter");
