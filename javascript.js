ee1730c581d85d92dfc0706ed49b2b8793b36348
rac640 <50179707+rac640@users.noreply.github.com>
2025-04-30 07:06:02 -0400

    Tested constructor

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// All of your book objects are going to be stored in an array, so you’ll need a constructor for books. 
+// Then, add a separate function to the script (not inside the constructor) that can:
+// 1.take some arguments, 2. create a book from those arguments, and 3. store the new book object into an array. 
+// Also, all of your book objects should have a unique id, which can be generated using crypto.randomUUID(). 
+// This ensures each book has a unique and stable identifier, preventing issues when books are removed or rearranged. 
+// Your code should look something like this (we’re showing only a basic skeleton without function parameters):
+
+
+const myLibrary = [];
+
+
+function Book(name ) {
+  // the constructor...
+  this.name = name;
+}
+
+const book1 = new Book("Harry Potter");
+
+alert(book1.name);
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
