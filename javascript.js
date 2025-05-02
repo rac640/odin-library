bfc595a5ab11315c61875ba9ed6a59ada1f778cd
rac640 <50179707+rac640@users.noreply.github.com>
2025-05-02 07:35:22 -0400

    Tested Putting libraryElement text content in divBox

--- javascript.js
+++ javascript.js
@@ -51,8 +51,10 @@ bookContents.textContent = libraryElements;
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of 
 for (const element of myLibrary){
   const divBox = document.createElement("div");
-  divBox.style.border = "thick solid #0000FF";
+  divBox.style.border = "1px solid black";
   bookContents.appendChild(divBox);
+
+  divBox.textContent = libraryElements;
   // AppendChild 
 }
 
